ecc00853c1c88744002aae68faba4170038692cf
rahulkfernandes <rahulkfernandes@gmail.com>
2026-02-04 13:46:18 -0500

    usage updated

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 
 To run data collection pipeline:
 ```
-python -m scripts.run_data_collection
+python -m scripts.run_data_collection --num_pkgs <NUM_PACKAGES>
 ```
--- scripts/run_data_collection.py
+++ scripts/run_data_collection.py
@@ -1,9 +1,20 @@
 import os
+import argparse
 from scripts.utils import load_config
 from src.data_collection.pipeline import run_collection_pipeline
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Data Collection Pipeline To Download Top PyPI and NPM Packages'
+    )
+
+    parser.add_argument(
+        '-n',
+        '--num_pkgs',
+        type=int,
+        help="Number of benign packages to be downloaded."
+    )
+    args = parser.parse_args()
     paths_config = load_config(os.path.join('config', 'paths.json'))
-
-    run_collection_pipeline(10000, paths_config)  # Change to required number of packages
-
+    
+    run_collection_pipeline(args.num_pkgs, paths_config)  # Change to required number of packages
